e1f777ac53b96a5e44f032638f7bf5d1dd26de5b
Samarth Upare <samarth.upare@redtra.com>
2024-12-14 11:05:15 +0530

    cost calculations

--- project_estimation/redtra_project_estimation/doctype/project_estimation/project_estimation.py
+++ project_estimation/redtra_project_estimation/doctype/project_estimation/project_estimation.py
@@ -6,4 +6,13 @@ from frappe.model.document import Document
 
 
 class ProjectEstimation(Document):
-	pass
+	def before_save(self):
+		total_material_cost = 0
+		total_labour_cost = 0
+		for item in self.items:
+			total_material_cost += item.amount
+			total_labour_cost += item.total_man_power_hours
+		self.total_material_cost = total_material_cost
+		self.total_labour_cost = total_labour_cost
+
+		
